098fe5350af41e6bb8c3721c2f4af527a8c87ad7
Екатерина <ekaterina.zalygina@mail.ru>
2024-01-09 09:08:45 +0300

    Домашняя работа 2.8

--- index.js
+++ index.js
@@ -516,3 +516,116 @@
 //     alert('Вы не угадали ни одного элемента')
 // }
 
+
+
+//Домашняя работа 2.8
+
+//Задание 1
+// const people = [
+//     { name: 'Глеб', age: 29 },
+//     { name: 'Анна', age: 17 },
+//     { name: 'Олег', age: 7 },
+//     { name: 'Оксана', age: 47 }
+//  ];
+//  people.sort(function (a, b) {
+//    if (a.age > b.age) {
+//      return 1;
+//    }
+//    if (a.age < b.age) {
+//      return -1;
+//    }
+//    if (a.age = b.age) {
+//    return 0;
+//    }
+//  });
+//  console.log(people);
+
+//Задание 2
+// function isPositive(num) {
+//     return num > 0;
+//     }
+
+// function filter(arr, isPositive) {
+//     let result = [];
+//     arr.forEach((el) => {
+//         if(isPositive(el)) {
+//         result.push(el);
+//         }
+//     });
+//     return result 
+// }
+
+// console.log(filter([3, -4, 1, 9], isPositive));
+
+
+// function isMale(floor) {
+//     return floor.gender === 'male';
+// }
+
+// function filter(people, isMale) {
+
+//     let result = [];
+//     people.forEach((el) => {
+//         if(isMale(el)) {
+//         result.push(el);
+//         }
+//     });
+//     return result 
+// }
+    
+// const people = [
+//        {name: 'Глеб', gender: 'male'},
+//        {name: 'Анна', gender: 'female'},
+//        {name: 'Олег', gender: 'male'},
+//        {name: 'Оксана', gender: 'female'}
+// ];
+    
+// console.log(filter(people, isMale)); 
+
+//Задание 3
+// const timer = (deadline) => {
+// 	let time = deadline;
+// 	console.log(time);
+// 	const interval = setInterval(() => {
+// 		time -= 1;
+//         console.log(new Date)
+// 		console.log(time);
+// 	}, 3000);
+
+
+// 	setTimeout(() => {
+//     clearInterval(interval);
+//     console.log('Время истекло!')
+//   }, deadline * 1000)
+// };
+
+// timer(30);
+
+//Задание 4
+// function delayForSecond(callback) {
+//     alert('Привет, Глеб!');
+//     setTimeout(delayForSecond, 1000);
+//   callback();
+// }
+
+// delayForSecond(function () {
+//   console.log('Привет, Глеб!');
+// })
+
+//Задание 5
+// function delayForSecond(cb) {
+//     setTimeout(() => {
+//         console.log('Прошла одна секунда');
+// 				if(cb) { 	cb(); }
+
+//     }, 1000)
+// }
+
+// function sayHi (name) {
+//     console.log(`Привет, ${name}!`);
+// }
+
+// // Код выше менять нельзя
+
+// // Нужно изменить код ниже:
+// delayForSecond(() => sayHi('Глеб'))
